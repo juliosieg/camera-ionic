c9a5f0d88072d523c0af8de56d4aa20e154c039b
Julio Sieg <131502@upf.br>
2018-05-19 01:48:29 -0300

    Camera

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { Camera, CameraOptions } from '@ionic-native/camera';
+
 
 @Component({
   selector: 'page-home',
@@ -7,8 +9,29 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController, public camera: Camera) {
 
   }
+
+  public imgBase64;
+
+  tirarFoto(){
+    const options: CameraOptions = {
+      quality: 100,
+      destinationType: this.camera.DestinationType.DATA_URL,
+      encodingType: this.camera.EncodingType.JPEG,
+      mediaType: this.camera.MediaType.PICTURE
+    }
+
+    this.camera.getPicture(options).then((imageData) => {
+      
+      this.imgBase64 = 'data:image/jpeg;base64,' + imageData;
+    }, (err) => {
+      console.log("Error:"+err);
+    });  
+  }
+
+
+  
 
 }
